672c109d94137f672a76e633f4199b6d70a922bf
SalnikovAleksey <lexasalnikov@gmail.com>
2024-02-04 18:03:43 +0500

    Init project

---
+++ src/main/java/ru/salnikov/motjobmultitool/MotJobmultitoolApplication.java
@@ -0,0 +1,13 @@
+package ru.salnikov.motjobmultitool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotJobmultitoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotJobmultitoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/salnikov/motjobmultitool/MotJobmultitoolApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.salnikov.motjobmultitool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotJobmultitoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
